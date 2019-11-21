538ea8527e0671a74972c9734880cd67319e7085
lqq <1357441309@qq.com>
2019-11-21 15:33:23 +0800

    commit

---
+++ springboot-activiti6/src/test/java/org/freeatalk/freeone/springboot/activiti6/SpringbootActiviti6ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.freeatalk.freeone.springboot.activiti6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootActiviti6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
